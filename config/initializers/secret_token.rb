6ec59ac4211287760f0917f67212cd297cdd469e
nadim <nadim@alshurafa.co>
2013-09-20 12:31:06 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorCode::Application.config.secret_token = 'f9ad1f93cfe3f5a80942e4d17cfbc11e7acbf8afa92da8dbdec078eee247c44558b100d318d3e14e17e946e22d2fd2ef971c0c0b396d9c51c8c660bf4dc24d3c'
